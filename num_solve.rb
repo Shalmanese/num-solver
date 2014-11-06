ccfeadb4ce969b73cdf023deb4f0ff14462ce50e
Xianhang Zhang <hang@bumblebeelabs.com>
2014-11-06 14:39:45 -0800

    Improved readability

--- num_solve.rb
+++ num_solve.rb
@@ -1,48 +1,50 @@
-@num_solns
+@num_tested
+@num_solutions
 
-# UI for the reducer
+# UI for the pick_twor
 def solve ()
-  @num_solns = 0
+  @num_tested = 0
+  @num_solutions = 0
   print "Enter Answer: "
   answer = gets.chomp.to_i
-  puts "Enter Numbers:"
-  es = []
-  6.times{es << gets.chomp.to_i}
+  print "Enter Numbers:\n"
+  numbers = []
+  6.times{numbers << gets.chomp.to_i}
   puts "Solving..."
-  reduce(es, answer, "")
-  @num_solns
+  pick_two(numbers, answer, "")
+  [@num_tested, @num_solutions]
 end
 
-# Picks all pair combos from es to combine
-def reduce (es, answer, solution)
-  return if es.length < 2 # Terminate recursion
-  for i in 0..es.length-1
-    for j in i+1..es.length-1
-      res = es.clone #remaining elements
-      b = res.delete_at(j) #delete j first so i is unaffected
-      a = res.delete_at(i)
+# Picks all pair combos from numbers to pick_op
+def pick_two (numbers, answer, solution)
+  return if numbers.length < 2 # Terminate if unable to pick
+  for i in 0..numbers.length-1
+    for j in i+1..numbers.length-1
+      remaining = numbers.clone #remaining elements
+      b = remaining.delete_at(j) #delete j first so i is unaffected
+      a = remaining.delete_at(i)
       # Since answer must be positive, we enforce a > b
-      combine([a,b].max, [a,b].min, res, answer, solution)
+      pick_op([a,b].max, [a,b].min, remaining, answer, solution)
     end
   end  
 end
 
 # Iterates through all 4 mathematical operations to test
-def combine (a, b, es, answer, solution)
-  test(a, b, "+", a+b, es, answer, solution.clone)
-  test(a, b, "-", a-b, es, answer, solution.clone)
-  test(a, b, "*", a*b, es, answer, solution.clone)
-  if (b != 0 && a % b == 0)
-    test(a, b, "/", a/b, es, answer, solution.clone)
-  end
+def pick_op (a, b, numbers, answer, solution)
+  test(a, b, "+", a+b, numbers, answer, solution.clone)
+  test(a, b, "-", a-b, numbers, answer, solution.clone)
+  test(a, b, "*", a*b, numbers, answer, solution.clone)
+  test(a, b, "/", a/b, numbers, answer, solution.clone) if (b != 0 && a % b == 0) #avoid divide by zero and fractions
 end
 
-def test(a, b, op, ab, es , answer, solution)
+def test(a, b, op, ab, numbers , answer, solution)
+  @num_tested += 1
   solution << "#{a} #{op} #{b} = #{ab}\n"
   if (ab == answer)
-    @num_solns += 1
-    print "\nSolution \##{@num_solns}:\n#{solution}"
+    @num_solutions += 1
+    print "\nSolution \##{@num_solutions}:\n#{solution}"
   else
-    reduce(es.clone.unshift(ab), answer, solution)
+    #put the result back into number and recurse
+    pick_two(numbers.clone.unshift(ab), answer, solution)
   end
 end
